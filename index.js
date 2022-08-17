9c912cf2391c6b1306f30554a8f83a70bf1fce32
dennis027 <machariad196@gmail.com>
2022-08-17 17:27:00 +0300

    updates

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-let a = function repeatorNumber(array){
+
+let A = function repeatorNumber(array){
     return array.sort((a,b) =>
           array.filter(v => v===a).length - array.filter(v => v===b).length
     ).pop();
    
 }
+function dominator(A) {
+    const array = []
+    for (let i=0; i<A.length; i++){
+      if (!array[A[i]])
+        array[A[i]] = 1
+        
+      else
+        array[A[i]]++
+      if (array[A[i]] > A.length/2)
+      return A.sort((a,b) =>
+      A.filter(v => v===a).length - A.filter(v => v===b).length
+).pop();
+
+
+        
+    }
+    return -1
+}
 
 
 
 
-
-console.log(a([1, 1, 3,3,3, 6,7,1,3,3])); 
+console.log(dominator([1, 1, 3,3,3,3, 6,7,1,3,3])); 
